fd62fd80d0c72675b24c7be0667130f98bbd6d57
Aarick-F <zeflyndesign@gmail.com>
2018-03-13 18:49:56 -0400

    moved pokemon data to its own file, adjusted some styling

---
+++ assets/javascript/pokemonData.js
@@ -0,0 +1,47 @@
+
+  const pokemonList = [
+    {
+      name: "Bulbasaur",
+      image: "<img class='pokemonPic' src='assets/images/001.png'>"
+    },
+    {
+      name: "Ivysaur",
+      image: "<img class='pokemonPic' src='assets/images/002.png'>"
+    },
+    {
+      name: "Venusaur",
+      image: "<img class='pokemonPic' src='assets/images/003.png'>"
+    },
+    {
+      name: "Charmander",
+      image: "<img class='pokemonPic' src='assets/images/004.png'>"
+    },
+    {
+      name: "Charmeleon",
+      image: "<img class='pokemonPic' src='assets/images/005.png'>"
+    },
+    {
+      name: "Charizard",
+      image: "<img class='pokemonPic' src='assets/images/006.png'>"
+    },
+    {
+      name: "Squirtle",
+      image: "<img class='pokemonPic' src='assets/images/007.png'>"
+    },
+    {
+      name: "Wartortle",
+      image: "<img class='pokemonPic' src='assets/images/008.png'>"
+    },
+    {
+      name: "Blastoise",
+      image: "<img class='pokemonPic' src='assets/images/009.png'>"
+    },
+    {
+      name: "Pikachu",
+      image: "<img class='pokemonPic' src='assets/images/025.png'>"
+    },
+    {
+      name: "Raichu",
+      image: "<img class='pokemonPic' src='assets/images/026.png'>"
+    }
+  ];
