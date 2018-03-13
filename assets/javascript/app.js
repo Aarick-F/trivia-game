4f1b6abb38107c3d768ccdf0dbdcdbfe4703cc00
Aarick-F <zeflyndesign@gmail.com>
2018-03-13 18:31:07 -0400

    added some data and logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,18 +6,68 @@ $(document).ready(() => {
   const pokemon = $("<div id='pokemon'></div>");
   const infoDisplay = $("<div id='infoDisplay'></div>");
   const answer = $("<div id='answer' class='info'></div>");
-  const timer = $("<div id='timer' class='info'></div>");
+  const timer = $("<div id='timer' class='info'>10</div>");
   const guessesLeft = $("<div id='guessesLeft'></div>");
   const answersField = $("<div id='answersField'></div>");
   // BEGINNING SCENE
   const beginning = $("#content").html();
   // =======================================================
 
+  // DATA SET
+  const pokemonList = [
+    {
+      name: "Bulbasaur",
+      image: "<img class='pokemonPic' src='assets/images/001.png'>"
+    },
+    {
+      name: "Ivysaur",
+      image: "<img class='pokemonPic' src='assets/images/002.png'>"
+    },
+    {
+      name: "Venusaur",
+      image: "<img class='pokemonPic' src='assets/images/003.png'>"
+    },
+    {
+      name: "Charmander",
+      image: "<img class='pokemonPic' src='assets/images/004.png'>"
+    },
+    {
+      name: "Charmeleon",
+      image: "<img class='pokemonPic' src='assets/images/005.png'>"
+    },
+    {
+      name: "Charizard",
+      image: "<img class='pokemonPic' src='assets/images/006.png'>"
+    },
+    {
+      name: "Squirtle",
+      image: "<img class='pokemonPic' src='assets/images/007.png'>"
+    },
+    {
+      name: "Wartortle",
+      image: "<img class='pokemonPic' src='assets/images/008.png'>"
+    },
+    {
+      name: "Blastoise",
+      image: "<img class='pokemonPic' src='assets/images/009.png'>"
+    },
+    {
+      name: "Pikachu",
+      image: "<img class='pokemonPic' src='assets/images/025.png'>"
+    },
+    {
+      name: "Raichu",
+      image: "<img class='pokemonPic' src='assets/images/026.png'>"
+    }
+  ];
+
   let game,
-      wrongGuesses,
+      guesses,
       score,
       seconds,
-      pickedPokemon;
+      pickedPokemon,
+      pokemonPicture,
+      pokemonName;
 
   // APP START
   // =========
@@ -28,10 +78,12 @@ $(document).ready(() => {
     $("#content").hide()
     .html(beginning)
     .fadeIn(1500);
-    wrongGuesses = 0;
+    guesses = 3;
     game = false;
     score = 0;
     pickedPokemon = [];
+    pokemonName = undefined;
+    pokemonPicture = undefined;
 
     $("#startButton").on("click", function() {
       game = true;
@@ -45,26 +97,49 @@ $(document).ready(() => {
 
   function round() {
     if(game) {
+      getPokemon();
       seconds = 10;
       const timer = setInterval(function() {
         seconds--;
+        $("#timer").html(seconds);
         console.log(seconds);
         if(seconds === 0) {
           clearInterval(timer);
-          wrongGuesses++;
-          if(wrongGuesses === 3) {
+          $("#answer").hide().html("It's " + pokemonName).fadeIn(1500);
+          $(".pokemonPic").addClass("reveal");
+          guesses--;
+          $("#guessesLeft").html(guesses);
+          if(guesses === 0) {
             // GAME OVER SCENE
           } else {
+            setTimeout(round, 3000);
             // ROUND RESTART
           }
         }
       }, 1000);
+      $("#answer").html("?");
+      $("#guessesLeft").html(guesses);
+      $("#pokemon").html(pokemonPicture);
+      $(".pokemonPic").hide().fadeIn(2000);
 
       $("#pokemon").on("click", function() {
-        clearInterval(timer);
-        init();
+        $(".pokemonPic").addClass("reveal");
       });
     }
+  }
+
+  function getPokemon() {
+    pokemonName = undefined;
+    pokemonPicture = undefined;
+    while(pokemonName == undefined) {
+      let picker = Math.floor(Math.random() * pokemonList.length);
+      if(pickedPokemon.indexOf(pokemonList[picker].name) == -1) {
+        pickedPokemon.push(pokemonList[picker].name);
+        pokemonPicture = pokemonList[picker].image;
+        pokemonName = pokemonList[picker].name;
+      }
+    }
+    console.log(pokemonName);  
   }
 
 });
